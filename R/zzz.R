ed7f2bfeb7064045746beec0c25a96dda9da83a9
Christoph Sax <christoph.sax@gmail.com>
2025-09-11 12:09:02 +0200

    line length

--- R/zzz.R
+++ R/zzz.R
@@ -3,7 +3,10 @@ register_ggplot_blocks <- function() {
   register_blocks(
     "new_chart_block",
     name = "Chart",
-    description = "Create any chart type using ggplot2 with dynamic visualization selector",
+    description = paste0(
+      "Create any chart type using ggplot2 with ",
+      "dynamic visualization selector"
+    ),
     category = "plot",
     package = utils::packageName(),
     overwrite = TRUE
